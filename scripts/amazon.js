a507c7e45be3422afecc2a5140336a8c394cc97b
Narasimha <kondanarasimha123@gmail.com>
2024-01-13 18:13:10 +0530

    make quantity selector interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -68,12 +68,15 @@
         }
       });
 
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
+      const quantity = Number(quantitySelector.value)
+
       if(matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: quantity
         })
       }
       let cartQuantity = 0;
