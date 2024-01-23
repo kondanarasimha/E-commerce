9c3732de472533bacc8dc54b6d3e486f4d7259aa
Narasimha <kondanarasimha123@gmail.com>
2024-01-23 14:55:10 +0530

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,20 @@
-  export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+  export let cart = JSON.parse(localStorage.getItem('cart'));
+
+  if(!cart) { //if cart is empty by using this defulate
+    [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1
+    }];
   }
-];
+  
+  
+  function saveLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
 
   export function addToCart(productId,quantity) {
     let matchingItem;
@@ -25,6 +33,7 @@
           quantity //productId: productId {Destructuring}
         })
       }
+      saveLocalStorage();
   }
 
   export function removeFromCart(productId) {
@@ -36,5 +45,5 @@
       }
     })
     cart = newCart
-
+    saveLocalStorage();
   }
