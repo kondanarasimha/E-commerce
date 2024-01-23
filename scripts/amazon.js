53a440f88fd8fd24edef35e211e7fd19c309a763
Narasimha <kondanarasimha123@gmail.com>
2024-01-23 12:33:53 +0530

    import products from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
   import {cart} from '../data/cart.js';
+  import {products} from '../data/products.js';
   
   //products from products.js.('it run's first because of products.js was in first)
   let productsHTML = ''; 
