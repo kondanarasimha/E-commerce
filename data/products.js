0c65b5a67a7547afa8a968270f633a8e3be6e2ea
Narasimha <kondanarasimha123@gmail.com>
2024-05-10 10:27:31 +0530

    Learn more details about classes and this keyword

--- data/products.js
+++ data/products.js
@@ -57,6 +57,37 @@ export function getProduct(productId) {
   }
  }
 
+/*
+ const date = new Date();
+ console.log(date);
+ console.log(date.toLocaleDateString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
+};
+*/
+
+/*
+function logThis() {
+  console.log(this);
+}
+
+logThis();
+logThis.call('hello');
+
+const object3 = {
+  method: ()=> {
+    console.log(this);
+  }
+}
+
+object3.method();
+*/
 
  export const products = [ //products is a object(intances of Product class).
   {
