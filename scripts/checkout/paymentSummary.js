8ac0c0d67c4a86a0b7ce942cbeee5ae3aafdd340
Narasimha <kondanarasimha123@gmail.com>
2024-05-23 13:09:55 +0530

    Create order using backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct, products } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOption.js";
 import { fromatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -60,11 +61,38 @@ export function renderPaymentSummary() {
       </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary 
+      js-place-order
+    ">
       Place your order
     </button>
     `;
 
     document.querySelector('.js-payment-summary')
       .innerHTML = paymentSummaryHTML;
-}
+
+
+    document.querySelector('.js-place-order')
+      .addEventListener('click',async()=> {
+        try {
+
+          const respones = await fetch('https://supersimplebackend.dev/orders', {
+            method: 'POST',
+            headers: {
+              'Content-type':'application/json' //it tell's more details of request.
+            },
+            body: JSON.stringify({
+              cart: cart //backend accepts the data with a name cart, and the other cart(value) it contains the cart array.
+            })
+          });
+  
+          const order = await respones.json(); //onces the data sent to the backend it gives the respoes of the data what we send.
+          addOrder(order); //The respones data was storing into the addOrder function.
+
+        } catch(error) {
+          console.log('Unexpected error. Try again later');
+        }
+
+        window.location.href = 'orders.html';
+      });
+};
